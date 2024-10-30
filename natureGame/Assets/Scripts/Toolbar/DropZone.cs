961d0122312366d87975a610f40ee095354e683e
oskaroskaroskaroskaroskar <ZQ21JJ@student.aau.dk>
2024-10-30 16:33:19 +0100

    Basic class structure

---
+++ natureGame/Assets/Scripts/Toolbar/DropZone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropZone
+{
+    float width;
+    float height;
+    float xPos;
+    float yPos;
+    public DropZone(float width, float height, float xPos, float yPos)
+    {
+        this.width = width;
+        this.height = height;
+        this.xPos = xPos;
+        this.yPos = yPos;
+
+    }
+}
