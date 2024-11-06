bac35f71feb90632d89f6fc0f40d6b9f8005ce55
Oscshup <51968074+Oscshup@users.noreply.github.com>
2024-11-06 18:06:42 +0100

    TrashPicker

--- natureGame/Assets/Scripts/Toolbar/Item.cs
+++ natureGame/Assets/Scripts/Toolbar/Item.cs
@@ -7,13 +7,16 @@ public abstract class Item : MonoBehaviour
     public Camera cam;
     public GameObject gameobject;
     public abstract void OnClick();
+
+
     void OnMouseDown ()
     {
         OnClick();
     }
+
     void Start ()
     {
-       
+
     }
 
 }
--- natureGame/Assets/Scripts/Toolbar/MouseStuckItem.cs
+++ natureGame/Assets/Scripts/Toolbar/MouseStuckItem.cs
@@ -4,8 +4,47 @@ using UnityEngine;
 
 public abstract class MouseStuckItem : Item
 {
+
+
+    private bool isStuckToMouse = false;
+    public GameObject trashPicker;
+
+    private void Start()
+    {
+        // Initialize the red object, make it initially inactive
+        trashPicker = Instantiate(gameObject); // Duplicate this object
+        trashPicker.GetComponent<Renderer>().material.color = Color.red; // Set color to red
+        trashPicker.SetActive(false); // Hide the red object initially
+
+    }
+
+
     public override void OnClick()
     {
 
+        if (isStuckToMouse)
+        {
+            isStuckToMouse = true;
+            trashPicker.transform.position = transform.position;
+            trashPicker.SetActive(true);
+        }
+        else
+        {
+            isStuckToMouse = false;
+            trashPicker.SetActive(false);
+        }
+
+
+    }
+
+
+    private void TrashPickerUpdate()
+    {
+        if (isStuckToMouse)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = -Camera.main.transform.position.z;
+            transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
     }
 }
