d0bd53082596beec770a46c147c647db8882291e
Oscshup <51968074+Oscshup@users.noreply.github.com>
2024-11-18 12:50:04 +0100

    oil rotate

--- natureGame/Assets/Scripts/Toolbar/OiilItem.cs
+++ natureGame/Assets/Scripts/Toolbar/OiilItem.cs
@@ -7,6 +7,9 @@ public class OiilItem : PouringItem
     public GameObject oilStain;
     float pourSpeed;
     float EnvInfl;
+    float degrees = 85;
+    
+
     public override void OnStart()
     {
         pourSpeed = 0.2f;
@@ -19,6 +22,9 @@ public class OiilItem : PouringItem
     {
         if (oilStain.transform.localScale.x<1)
         {
+            Vector3 to = new Vector3(0, 0, degrees);
+            transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+            
             oilStain.transform.localScale += new Vector3(Time.deltaTime*pourSpeed, Time.deltaTime*pourSpeed, 0);
             oilStain.GetComponent<OilPool>().influence += Time.deltaTime * pourSpeed* EnvInfl;
             Camera.main.GetComponent<GameManager>().GetInfluence();
